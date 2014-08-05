03d2ce35d07417317b138d61a81cd3e2dccd5ec5
jacogd <jacogd@gmail.com>
2014-08-05 09:12:21 +0200

    Skynet Tracking

---
+++ js/index.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+
+
+    $("#btnSearch").click(function () {
+
+        $("#divDisplay").hide();
+
+
+        $("#divDisplay").html('<object data="http://www.crossroads.co.za/tracking2/ajax/Default.aspx?id=trackDetail&type=way&wb=' + $("#txtWbnr").val() + '&suser=webuser"/>');
+        $("#divDisplay").show();
+        //        $.ajax({
+        //            //method: "get",
+        //            //cache: false,
+        //            url: "http://www.crossroads.co.za/tracking2/ajax/Default.aspx?id=trackDetail&type=way&wb=019909095565&suser=webuser",
+        //            dataType: 'jsonp',
+        //            success: function (jsonp) {
+        ////                $("#divDisplay").html(data);
+        ////                $("#divDisplay").show();
+        //            }
+        //        });
+
+    });
+});
+
